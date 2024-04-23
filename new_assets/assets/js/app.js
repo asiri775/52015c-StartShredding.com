4c7a26da43ff0d2d6c44c1a42c5eb4089273ea80
Jason David <jasondavid1217@outlook.com>
2024-04-23 04:51:00 -0700

    fix errors in signature function

--- new_assets/assets/js/app.js
+++ new_assets/assets/js/app.js
@@ -135,9 +135,18 @@ saveJPGButton.addEventListener("click", () => {
 
       var xhr = new XMLHttpRequest();
       xhr.onload = function() {
-        window.parent.document.getElementById('save_flag').value = 1;
+        
         // result = JSON.parse(this.responseText);
-        alert('Signature saved succeessfully');
+        if(this.status == 200){
+          window.parent.document.getElementById('save_flag').value = 1;
+          console.log(this.responseText);
+          alert('Signature saved succeessfully');
+        }
+        else {
+          
+          alert('Signature save failed');
+        }
+        
       }
       xhr.open("POST", "/save_signature");
       xhr.send(formData);
